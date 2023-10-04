26c9eaa8768a9a70eb8111b66beea776bd563cdd
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-04 21:29:27 +0530

    useQuery() hook to fetch api data

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import axios from 'axios';
+import { Fragment } from 'react';
+import { useQuery } from 'react-query';
+
+const fetchSuperHeroes = () => {
+    return axios.get('https://apidata-zkgz.onrender.com/superheroes')
+}
 
 const RQSuperHeroes = () => {
+    const {isLoading, data} = useQuery('super-heroes', fetchSuperHeroes);
+
+    if(isLoading){
+        return <h2>Loading...</h2>
+    }
+
   return (
-    <div>
-      
-    </div>
+    <Fragment>
+      <h2>RQ Super Heroes Page</h2>
+      {
+        data?.data.map((hero) => {
+            return (
+                <div key={hero.id}>
+                    <p>{hero.name}</p>
+                </div>
+            )
+        })
+      }
+    </Fragment>
   )
 }
 
