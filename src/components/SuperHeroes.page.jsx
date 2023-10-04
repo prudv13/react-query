cab29a5a219dd357e53843ba70def18c6634b22d
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-04 21:46:39 +0530

    traditional error handling

--- src/components/SuperHeroes.page.jsx
+++ src/components/SuperHeroes.page.jsx
@@ -4,16 +4,25 @@ import React, { useEffect, useState } from 'react'
 const SuperHeroes = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [data, setData] = useState([]);
+    const [error, setError] = useState('');
 
     useEffect(() => {
         axios.get('https://apidata-zkgz.onrender.com/superheroes').then((res) => {
             setData(res.data)
             setIsLoading(false)
         })
+        .catch(err => {
+            setError(err.message);
+            setIsLoading(false);
+        })
     }, []);
 
     if(isLoading){
         return <h2>Loading...</h2>
+    }
+
+    if(error) {
+        return <h2>{error}</h2>
     }
     return (
     <div>
