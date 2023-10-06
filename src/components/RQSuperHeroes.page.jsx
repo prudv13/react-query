14ed9f1e884e1c5055a11afc996ff151c2838e9c
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-06 09:06:17 +0530

    custom query hook

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -1,10 +1,7 @@
 import axios from 'axios';
 import { Fragment } from 'react';
 import { useQuery } from 'react-query';
-
-const fetchSuperHeroes = () => {
-    return axios.get('https://apidata-zkgz.onrender.com/superheroes')
-}
+import { useSuperHeroesData } from '../hooks/useSuperHeroesData';
 
 const RQSuperHeroes = () => {
 
@@ -15,25 +12,8 @@ const RQSuperHeroes = () => {
   const onError = () => {
     console.log("Perform side effect after data fetching");
   }
-  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
-      "super-heroes",
-      fetchSuperHeroes,
-      {
-        //cacheTime: 5 minutes,
-        //staleTime: 0 seconds,
-        //refetchOnMount: true, false, 'always',
-        //refetchOnWindowFocus: true, false, 'always',
-        //refetchInterval: 2000,
-        //refetchIntervalInBackground: true,
-        //enabled: false,
-        onSuccess,
-        onError,
-        select: (data) => {
-          const superHeroNames = data.data.map(hero => hero.name);
-          return superHeroNames;
-        }
-      }
-  );
+
+  const { isLoading, data, isError, error, isFetching, refetch } = useSuperHeroesData(onSuccess, onError);
 
   console.log(`Loading: ${isLoading}, Fetching: ${isFetching}`);
 
---
+++ src/hooks/useSuperHeroesData.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchSuperHeroes = () => {
+    return axios.get('https://apidata-zkgz.onrender.com/superheroes')
+}
+
+export const useSuperHeroesData = (onSuccess, onError) => {
+    return useQuery(
+        "super-heroes",
+        fetchSuperHeroes,
+        {
+            //cacheTime: 5 minutes,
+            //staleTime: 0 seconds,
+            //refetchOnMount: true, false, 'always',
+            //refetchOnWindowFocus: true, false, 'always',
+            //refetchInterval: 2000,
+            //refetchIntervalInBackground: true,
+            //enabled: false,
+            onSuccess,
+            onError,
+            select: (data) => {
+              const superHeroNames = data.data.map(hero => hero.name);
+              return superHeroNames;
+            }
+        }
+      );
+}
