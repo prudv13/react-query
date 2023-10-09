6f62cbae513c827d8bd05065d375fc0292ceeb8a
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-09 17:27:04 +0530

    traditional error handling

--- src/components/SuperHeroes.jsx
+++ src/components/SuperHeroes.jsx
@@ -4,17 +4,18 @@ import React, { useEffect, useState } from 'react'
 const SuperHeroes = () => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState('');
 
     useEffect(() => {
       (async() => {
         try {
-          const response = await axios.get('http://localhost:4000/superheroes');
+          const response = await axios.get('http://localhost:4000/superheroes1');
           const superheroesdata = await response.data;
           setData(superheroesdata);
           setIsLoading(false);
         }
         catch(error){
-          console.log(error);
+          setError(error.message);
           setIsLoading(false);
         }
       })();
@@ -23,6 +24,10 @@ const SuperHeroes = () => {
     if(isLoading) {
         return <h2 style={{margin: "30px"}}>Loading...</h2>
     }
+
+    if(error) {
+      return <h2 style={{margin: "30px", color:"red"}}>{error}</h2>
+  }
   return (
     <div style={{margin: "30px"}}>
         <h2 style={{marginBottom: "20px"}}>Super Heroes</h2>
