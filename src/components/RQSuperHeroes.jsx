3d2fff890342724f80248cbd7599b712a3828cfa
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-09 17:09:52 +0530

    QueryClientProvider, QueryClient, useQuery, data, isLoading

--- src/components/RQSuperHeroes.jsx
+++ src/components/RQSuperHeroes.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+
+function fetchSuperHeroes(){
+    return axios.get('http://localhost:4000/superheroes')
+}
 
 const RQSuperHeroes = () => {
-  return (
-    <div>RQSuperHeroes</div>
+
+    const {data, isLoading} = useQuery(['super-heroes'], () => fetchSuperHeroes());
+    
+    if(isLoading) {
+        return <h2 style={{margin: "30px"}}>Loading...</h2>
+    }
+
+    return (
+    <div style={{margin: "30px"}}>
+        <h2 style={{marginBottom: "20px"}}>Super Heroes</h2>
+        <ul>
+            {
+                data?.data.map((hero) => {
+                    return <li style={{listStyle: "none"}} key={hero.id}>{hero.name}</li>
+                })
+            }
+        </ul>
+    </div>
   )
 }
 
