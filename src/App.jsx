574d2bde3e4ae2e0224d8660ff13a5ba07312df3
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-09 09:01:04 +0530

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-import React from 'react'
-import { Link, Route, Routes } from 'react-router-dom';
-import SuperHeroes from './components/SuperHeroes.page';
-import RQSuperHeroes from './components/RQSuperHeroes.page';
-import Home from './components/Home.page';
-import RQSuperHero from './components/RQSuperHero';
-import ParallelQueries from './components/ParallelQueries';
-import DynamicParallel from './components/DynamicParallel';
+
 
 const App = () => {
   return (
@@ -13,32 +6,9 @@ const App = () => {
 
       <nav>
         <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/super-heroes'>Traditional Super Heroes</Link>
-          </li>
-          <li>
-            <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-          </li>
-          <li>
-            <Link to='/rq-parallel'>RQ Parallel Queries</Link>
-          </li>
-          <li>
-            <Link to='/rq-dynamic-parallel'>Dynamic Parallel Queries</Link>
-          </li>
         </ul>
       </nav>
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/super-heroes' element={<SuperHeroes />} />
-        <Route path='/rq-super-heroes/:heroId' element={<RQSuperHero />} />
-        <Route path='/rq-super-heroes' element={<RQSuperHeroes />} />
-        <Route path='/rq-parallel' element={<ParallelQueries />} />
-        <Route path='/rq-dynamic-parallel' element={<DynamicParallel heroIds={[1, 3]} />} />
-      </Routes>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
