b9f24574f2d13d10d60176743ca553c143d8a660
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-09 17:40:38 +0530

    react query devtools

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
+    
   </React.StrictMode>
 );
